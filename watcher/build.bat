81d4c8a1fd056860e8b25ea121151c29a1bd79bd
anveloper <hitedin@gmail.com>
2026-05-15 10:13:31 +0900

    fix: requests 모듈 PyInstaller 번들 누락 추가

--- watcher/build.bat
+++ watcher/build.bat
@@ -5,7 +5,7 @@ REM 의존성 설치
 pip install -r requirements.txt
 
 REM PyInstaller로 exe 빌드 (단일 파일, watcher+agent 통합 GUI)
-pyinstaller --onefile --name label-printer --windowed --hidden-import win32print --hidden-import win32ui --hidden-import win32api --collect-data customtkinter --collect-submodules gui --add-data "assets/fonts;assets/fonts" main.py
+pyinstaller --onefile --name label-printer --windowed --hidden-import win32print --hidden-import win32ui --hidden-import win32api --hidden-import requests --hidden-import urllib3 --hidden-import certifi --hidden-import charset_normalizer --hidden-import idna --collect-data customtkinter --collect-submodules gui --add-data "assets/fonts;assets/fonts" main.py
 
 echo.
 echo 빌드 완료: dist\label-printer.exe
